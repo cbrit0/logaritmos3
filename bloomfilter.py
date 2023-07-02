dc6c51e33db9163b57c9138c5a89eeece61be2c7
cbrit0 <101942285+cbrit0@users.noreply.github.com>
2023-07-02 14:23:16 -0400

    better hash

--- bloomfilter.py
+++ bloomfilter.py
@@ -12,25 +12,32 @@ class BloomFilter:
             numero de funciones de hash
         M: bitarray
             el arreglo de bits
+        p: int
+            primo mayor que el tamaño del universo
     """
     def __init__(self, m, k):
         self.m = m
         self.k = k
         self.M = bitarray(m)
         self.M.setall(0)
-        self.p = 200003 # primo mayor que el tamaño del universo
-        self.a = random.randint(1, self.p)
+        self.p = 100000007
     
     def add(self, x):
         x = str_to_int(x)
-        for b in range(self.k):
-            j = ((self.a * x + b) % self.p) % self.m # usamos distintos hashes de la familia universal
+        for i in range(self.k):
+            random.seed(i)
+            a = random.randint(1, self.p-1)
+            b = random.randint(0, self.p-1)
+            j = ((a * x + b) % self.p) % self.m # usamos distintos hashes de la familia universal
             self.M[j] = 1
     
     def __contains__(self, y):
         y = str_to_int(y)
-        for b in range(self.k):
-            j = ((self.a * y + b) % self.p) % self.m
+        for i in range(self.k):
+            random.seed(i)
+            a = random.randint(1, self.p-1)
+            b = random.randint(0, self.p-1)
+            j = ((a * y + b) % self.p) % self.m
             if self.M[j] == 0:
                 return False
         return True
